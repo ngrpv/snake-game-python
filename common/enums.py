23ccce83aae373089ffc4a140c5852e254300278
Nazar Garipov <nazar.garipov475@gmail.com>
2022-04-04 15:00:05 +0500

    correct enums

--- common/enums.py
+++ common/enums.py
@@ -4,14 +4,14 @@ from prompt_toolkit.data_structures import Point
 
 
 class Direction(Enum):
-    Up = Point(0, -1),
-    Down = Point(0, 1),
-    Left = Point(-1, 0),
+    Up = Point(0, -1)
+    Down = Point(0, 1)
+    Left = Point(-1, 0)
     Right = Point(1, 0)
 
 
 class MapCellType(Enum):
-    Empty = 0,
-    Obstacle = 1,
-    Food = 2,
+    Empty = 0
+    Obstacle = 1
+    Food = 2
     Snake = 3
