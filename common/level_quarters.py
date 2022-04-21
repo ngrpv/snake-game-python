1f537d6bfa7813efd200325519eb87dfe0725668
nm101 <melnikovsm2@gmail.com>
2022-04-21 20:46:15 +0500

    Quick fix QuartersLevel snake spawn position

--- common/level_quarters.py
+++ common/level_quarters.py
@@ -1,12 +1,21 @@
 from common.level_empty import EmptyLevel
 from common.game_map import GameMap
 from common.enums import MapCellType
+from common.point import Point
+from common.snake import Snake
 
 
 class QuartersLevel(EmptyLevel):
     """Level with; map which divided into 4 regions, reachable by edges trespassing only"""
     def _create_map(self, width: int, height: int) -> GameMap:
         return QuartersMap(width, height)
+
+    def _create_snake(self, width: int, height: int) -> Snake:
+        x = width // 4
+        y = 3 * height // 4
+        head = Point(x, y)
+        tail = head + Point(2, 0)
+        return Snake(head, tail)
 
 
 class QuartersMap(GameMap):
